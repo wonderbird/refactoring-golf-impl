c02d9e0532231b40f1ffc96094df46c1c28bcc42
Stefan Boos <kontakt@boos.systems>
2022-11-21 05:31:32 +0100

    feat: hole3

--- app/src/test/java/refactoringgolf/TakeHomeCalculatorTest.java
+++ app/src/test/java/refactoringgolf/TakeHomeCalculatorTest.java
@@ -8,13 +8,13 @@ public class TakeHomeCalculatorTest {
 
     @Test
     public void canCalculateTax() throws Exception {
-        Integer first = new TakeHomeCalculator(10).netAmount(new TakeHomeCalculator.Pair<>(40, "GBP"), new TakeHomeCalculator.Pair<>(50, "GBP"), new TakeHomeCalculator.Pair<>(60, "GBP")).first;
+        Integer first = new TakeHomeCalculator(10).netAmount(new TakeHomeCalculator.Money(40, "GBP"), new TakeHomeCalculator.Money(50, "GBP"), new TakeHomeCalculator.Money(60, "GBP")).value;
         assertEquals(135, first.intValue());
     }
 
     @Test
     public void cannotSumDifferentCurrencies() throws Exception {
-        assertThrows(Incalculable.class, () -> new TakeHomeCalculator(10).netAmount(new TakeHomeCalculator.Pair<>(4, "GBP"), new TakeHomeCalculator.Pair<>(5, "USD")));
+        assertThrows(Incalculable.class, () -> new TakeHomeCalculator(10).netAmount(new TakeHomeCalculator.Money(4, "GBP"), new TakeHomeCalculator.Money(5, "USD")));
     }
 }
 
